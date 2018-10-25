d7a893a1a1c0b715beb2db1860853b9fc3bda08f
h3birth <h3.birth@gmail.com>
2018-10-25 20:38:01 +0900

    create project

---
+++ app/src/main/java/birth/h3/app/curl_kusegeapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package birth.h3.app.curl_kusegeapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
