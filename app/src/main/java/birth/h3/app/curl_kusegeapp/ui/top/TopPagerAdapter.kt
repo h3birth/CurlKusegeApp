171194e3d143d1f22295566b352587400350c6ee
h3birth <h3.birth@gmail.com>
2018-12-16 20:58:33 +0900

    viewpager return case

--- app/src/main/java/birth/h3/app/curl_kusegeapp/ui/top/TopPagerAdapter.kt
+++ app/src/main/java/birth/h3/app/curl_kusegeapp/ui/top/TopPagerAdapter.kt
@@ -3,15 +3,17 @@ package birth.h3.app.curl_kusegeapp.ui.top
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentPagerAdapter
 import android.util.Log
+import birth.h3.app.curl_kusegeapp.ui.weather.EmptyCityFragment
 import birth.h3.app.curl_kusegeapp.ui.weather.WeatherFragment
 
 class TopPagerAdapter(fm: android.support.v4.app.FragmentManager): FragmentPagerAdapter(fm) {
     private val tabTitles = arrayOf<CharSequence>("現在地", "地域1", "地域2", "地域3", "地域4", "地域5")
 
     override fun getItem(p0: Int): Fragment {
-        Log.d("vpposition", p0.toString())
-        val fragment = WeatherFragment(p0)
-        return fragment
+        return when(p0){
+            0 -> WeatherFragment(p0)
+            else -> EmptyCityFragment()
+        }
     }
 
     override fun getPageTitle(position: Int): CharSequence = tabTitles[position].toString()
