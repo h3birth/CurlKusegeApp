def671147912c13307856f244845d8bcc4fae689
h3birth <h3.birth@gmail.com>
2019-01-27 21:21:26 +0900

    goto RegisterCityActivity

--- app/src/main/java/birth/h3/app/curl_kusegeapp/ui/weather/EmptyCityFragment.kt
+++ app/src/main/java/birth/h3/app/curl_kusegeapp/ui/weather/EmptyCityFragment.kt
@@ -1,12 +1,15 @@
 package birth.h3.app.curl_kusegeapp.ui.weather
 
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import birth.h3.app.curl_kusegeapp.R
+import birth.h3.app.curl_kusegeapp.ui.registercity.RegisterCityActivity
+import kotlinx.android.synthetic.main.fragment_empty_city.*
 
 class EmptyCityFragment : androidx.fragment.app.Fragment() {
 
@@ -16,5 +19,12 @@ class EmptyCityFragment : androidx.fragment.app.Fragment() {
         return inflater.inflate(R.layout.fragment_empty_city, container, false)
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
 
+        button_register_city.setOnClickListener {
+            val intent = Intent(this.activity?.application, RegisterCityActivity::class.java)
+            startActivity(intent)
+        }
+    }
 }
