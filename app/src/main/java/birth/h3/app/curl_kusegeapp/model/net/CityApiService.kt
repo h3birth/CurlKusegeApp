fe6891ca198ee623059fde1aaf99ebad9d0a39a5
h3birth <h3.birth@gmail.com>
2019-02-16 15:50:45 +0900

    add searchview

--- app/src/main/java/birth/h3/app/curl_kusegeapp/model/net/CityApiService.kt
+++ app/src/main/java/birth/h3/app/curl_kusegeapp/model/net/CityApiService.kt
@@ -1,5 +1,14 @@
 package birth.h3.app.curl_kusegeapp.model.net
 
+import birth.h3.app.curl_kusegeapp.model.entity.SearchAddressResponse
+import io.reactivex.Single
+import retrofit2.http.GET
+import retrofit2.http.Path
+
 interface CityApiService{
+    @GET("public/api/address/search/{address}")
+    fun getAddress(
+        @Path("address") address: String
+    ): Single<SearchAddressResponse>
 
 }
--- app/src/main/java/birth/h3/app/curl_kusegeapp/ui/registercity/RegisterCityActivity.kt
+++ app/src/main/java/birth/h3/app/curl_kusegeapp/ui/registercity/RegisterCityActivity.kt
@@ -1,8 +1,8 @@
 package birth.h3.app.curl_kusegeapp.ui.registercity
 
 import android.os.Bundle
-import android.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import birth.h3.app.curl_kusegeapp.R
 import kotlinx.android.synthetic.main.activity_register_city.*
 
@@ -14,9 +14,21 @@ class RegisterCityActivity : AppCompatActivity() {
         setContentView(R.layout.activity_register_city)
 
         register_city_toolbar?.let{
-            it.setOnClickListener { this.finish() }
+            it.inflateMenu(R.menu.menu_search)
+            val searchView: SearchView = it.menu.findItem(R.id.search_city).actionView as SearchView
+            searchView.apply {
+                this.isIconified = false
+                this.queryHint = "地域を検索"
+                this.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+                    override fun onQueryTextChange(newText: String?): Boolean {
+                        return false
+                    }
+
+                    override fun onQueryTextSubmit(query: String?): Boolean {
+                        return false
+                    }
+                })
+            }
         } ?: IllegalAccessException("Toolbar cannot be null")
-
-
     }
 }
