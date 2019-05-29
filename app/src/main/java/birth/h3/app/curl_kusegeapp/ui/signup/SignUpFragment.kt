dd54098deb8e034f9747c96b01fc0c890335e4de
h3birth <h3.birth@gmail.com>
2019-05-30 00:48:12 +0900

    add SignupMessage postDelay

--- app/src/main/java/birth/h3/app/curl_kusegeapp/ui/signup/SignUpFragment.kt
+++ app/src/main/java/birth/h3/app/curl_kusegeapp/ui/signup/SignUpFragment.kt
@@ -2,16 +2,20 @@ package birth.h3.app.curl_kusegeapp.ui.signup
 
 
 import android.os.Bundle
+import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import birth.h3.app.curl_kusegeapp.CurlApp
 import birth.h3.app.curl_kusegeapp.R
+import birth.h3.app.curl_kusegeapp.databinding.FragmentSignUpBinding
 import birth.h3.app.curl_kusegeapp.ui.registercity.RegisterCityViewModel
 import kotlinx.android.synthetic.main.fragment_sign_up.*
+import timber.log.Timber
 import javax.inject.Inject
 
 
@@ -32,17 +36,24 @@ class SignUpFragment : Fragment() {
     @Inject
     lateinit var viewModel: SignUpViewModel
 
+    private lateinit var binding: FragmentSignUpBinding
     private val controller by lazy { SignUpController() }
+
+    private val handler = Handler()
+    private var runnable: Runnable? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_sign_up, container, false)
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         (context?.applicationContext as CurlApp).component.inject(this)
+        binding.setLifecycleOwner(this)
+        binding.viewModel = viewModel
 
         signup_recycler_view.apply {
             adapter = controller.adapter
@@ -53,6 +64,36 @@ class SignUpFragment : Fragment() {
     }
 
     private fun observe(){
-        controller.setData(viewModel.signupMessages)
+        runnable = Runnable {
+            viewModel.postMessage()?.let {
+                Timber.d("message is " + it.toString())
+                controller.setData(it)
+                signup_recycler_view.smoothScrollToPosition(it.lastIndex)
+                when(it.last().wait) {
+                    true -> {
+                        viewModel.buttonVisibility.postValue(View.VISIBLE)
+                        setButtonText(it.lastIndex)
+                    }
+                    false -> {
+                        handler.postDelayed(runnable, 1000)
+                    }
+                }
+            } ?: removeRunner()
+        }
+        handler.postDelayed(runnable, 1000)
+    }
+
+    private fun setButtonText(position: Int) = when(position) {
+        2 -> viewModel.setButtonText("さらさら", "ちょいくせ", "ちょうくせ")
+        else -> viewModel.setButtonText("", "", "")
+    }
+
+    private fun removeRunner() {
+        handler.removeCallbacks(runnable)
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        removeRunner()
     }
 }
--- app/src/main/java/birth/h3/app/curl_kusegeapp/ui/signup/SignUpViewModel.kt
+++ app/src/main/java/birth/h3/app/curl_kusegeapp/ui/signup/SignUpViewModel.kt
@@ -1,13 +1,36 @@
 package birth.h3.app.curl_kusegeapp.ui.signup
 
+import android.view.View
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import birth.h3.app.curl_kusegeapp.model.db.AppDatabase
 import birth.h3.app.curl_kusegeapp.model.entity.SignupMessage
 import birth.h3.app.curl_kusegeapp.model.net.UserApiService
 import birth.h3.app.curl_kusegeapp.model.net.WeatherApiService
+import timber.log.Timber
 import javax.inject.Inject
 
 class SignUpViewModel @Inject constructor(private val userApiService: UserApiService,
                                                 private val builder: AppDatabase) : ViewModel() {
+
+    val buttonVisibility: MutableLiveData<Int> = MutableLiveData<Int>().apply { value = View.GONE }
+    val firstButtonText: MutableLiveData<String> = MutableLiveData()
+    val secondButtonText: MutableLiveData<String> = MutableLiveData()
+    val thirdButtonText: MutableLiveData<String> = MutableLiveData()
     val signupMessages = SignupMessage.signUpMessages()
+    var postIndex: Int = 0
+
+    fun postMessage(): List<SignupMessage>? {
+        Timber.d("postIndex is ${this.postIndex} size is ${signupMessages.size + 1}")
+        if(this.postIndex >= signupMessages.size - 1) return null
+        val messages =  signupMessages.slice(0..this.postIndex)
+        this.postIndex++
+        return messages
+    }
+
+    fun setButtonText(firstText: String, secondText: String, thirdText: String) {
+        firstButtonText.postValue(firstText)
+        secondButtonText.postValue(secondText)
+        thirdButtonText.postValue(thirdText)
+    }
 }
