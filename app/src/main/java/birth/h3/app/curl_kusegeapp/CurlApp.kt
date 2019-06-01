ffdf4117c919940d853e0b62345d84f2127c365d
h3birth <h3.birth@gmail.com>
2019-06-02 00:51:13 +0900

    add setOnClickListener

--- app/src/main/java/birth/h3/app/curl_kusegeapp/CurlApp.kt
+++ app/src/main/java/birth/h3/app/curl_kusegeapp/CurlApp.kt
@@ -1,6 +1,9 @@
 package birth.h3.app.curl_kusegeapp
 
 import android.app.Application
+import android.net.Uri
+import androidx.browser.customtabs.CustomTabsIntent
+import androidx.core.content.ContextCompat
 import birth.h3.app.curl_kusegeapp.di.AppComponent
 import birth.h3.app.curl_kusegeapp.di.AppModule
 import birth.h3.app.curl_kusegeapp.di.DaggerAppComponent
@@ -15,5 +18,16 @@ class CurlApp : Application() {
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
         }
+    }
+
+    fun openWeb(pageUrl: String) {
+        CustomTabsIntent.Builder()
+                .setShowTitle(true)
+                .setToolbarColor(ContextCompat.getColor(this, R.color.colorPrimary))
+                .build().launchUrl(this, Uri.parse(pageUrl))
+    }
+
+    fun curlPageURI(uri: String){
+        this.openWeb(BuildConfig.CURL_URL + uri)
     }
 }
--- app/src/main/java/birth/h3/app/curl_kusegeapp/ui/signin/SignInFragment.kt
+++ app/src/main/java/birth/h3/app/curl_kusegeapp/ui/signin/SignInFragment.kt
@@ -1,13 +1,16 @@
 package birth.h3.app.curl_kusegeapp.ui.signin
 
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import birth.h3.app.curl_kusegeapp.CurlApp
 
 import birth.h3.app.curl_kusegeapp.R
+import birth.h3.app.curl_kusegeapp.ui.signup.SignUpActivity
 import kotlinx.android.synthetic.main.fragment_sign_in.*
 
 
@@ -23,6 +26,8 @@ class SignInFragment : Fragment() {
 
     val TAG = "SignInFragment"
 
+    private val curlApp by lazy { (context?.applicationContext as CurlApp) }
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -33,8 +38,16 @@ class SignInFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
+
         sign_in_toolbar.setOnClickListener {
             this.activity?.finish()
+        }
+
+        forget_password.setOnClickListener { curlApp.curlPageURI("forgetpassword") }
+
+        signup_goto.setOnClickListener {
+            val intent = Intent(this.context, SignUpActivity::class.java)
+            startActivity(intent)
         }
     }
 
